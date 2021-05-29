d0b0e9d6276e451be640facaf8d4a688127b15d3
khel29 <83513798+khel29@users.noreply.github.com>
2021-05-30 00:16:38 +0200

    Add files via upload

---
+++ passwords.py
@@ -0,0 +1,21 @@
+#flask related data
+pwd_secret_key_flask="**********"
+pwd_flask_port=0000
+
+# messages and password reset encryption decryption password
+pwd_enc_decryption="**********"
+
+#Mysql base related data
+pwd_mysql_database= "**********"
+pwd_mysql_port=0001
+pwd_mysql_user  = "**********"
+pwd_mysql_base ="**********"
+# e mails related data
+pwd_email = "**********"
+pwd_email_address= '"***@**.com'
+pwd_mail_user="***"
+pwd_smtp_port=465
+pwd_mail_server="mail.server"
+
+#admin user name
+pwd_admin_name="asmin name"
